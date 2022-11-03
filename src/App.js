4f7125b3de67e9fecf7df030ac0095b8a452cc4a
karanagrawal14 <karanagrawal1411@gmail.com>
2022-11-03 08:41:49 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Application  from './main';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,23 +6,33 @@ import { Switch, Route } from "react-router-dom";
 import Admin  from './Admin';
 import Login  from './login';
 import Signup from './Signup';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
 import ProtectedRoute2 from './ProtectedRoute2';
+import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 function App() {
   return (
     <div className="App">
         <>
     <Router>
+      <Header/>
       <Switch>
+        <Route exact path='/home' component={Home}/>
         <Route exact path="/signup" component={Signup}/>
          <ProtectedRoute2 exact path="/" component={Application}/> 
         <Route exact path="/login"><Login/></Route>
         <ProtectedRoute2 exact path="/admin" component={Admin} />
         <ProtectedRoute2 exact path="/books" component={Application}/>
+        <Route exact path='/contactus'  />
       </Switch>
-
+      <Footer/>
     </Router>
     </>
-    </div>
+    {/* <Header/> */}
+   
+    </div> 
+    
   );
 }
 
